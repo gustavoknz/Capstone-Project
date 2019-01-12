0449bd38d2d7c4fcb354d2b84aee65db2290b9ad
Gustavo Kieling <gustavoknz@gmail.com>
2019-01-11 23:22:45 -0400

    Favorite action completed

--- app/src/main/java/com/bora/gustavo/activities/GymActivity.java
+++ app/src/main/java/com/bora/gustavo/activities/GymActivity.java
@@ -1,10 +1,12 @@
 package com.bora.gustavo.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bora.gustavo.R;
 import com.bora.gustavo.activities.adapters.GymImagesRecyclerViewAdapter;
@@ -12,10 +14,14 @@ import com.bora.gustavo.helper.Utils;
 import com.bora.gustavo.models.EquipmentParcelable;
 import com.bora.gustavo.models.GymParcelable;
 import com.bora.gustavo.models.Vote;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Date;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,23 +29,27 @@ import butterknife.OnClick;
 
 public class GymActivity extends BackActivity {
     private static final String TAG = "GymActivity";
-    private boolean mIsFavorite;
-    private DatabaseReference mDatabase;
+    private boolean mIsFavorite = false;
+    private DatabaseReference mDatabaseVotes;
     private GymParcelable mGym;
 
     @BindView(R.id.gym_favorite)
     ImageView mFavoriteImage;
     @BindView(R.id.gym_recycler_view_equipments)
     RecyclerView mRecyclerViewEquipments;
+    @BindView(R.id.gym_accessible_text)
+    TextView mAccessibleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gym);
         ButterKnife.bind(this);
-
         mGym = getIntent().getParcelableExtra(Utils.PARAM_GYM);
         Log.d(TAG, "Got mGym as parameter: " + mGym);
+
+        setFavoriteImage();
+        setAccessibleText();
 
         mRecyclerViewEquipments.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         String[] data = new String[mGym.getEquipmentList().size()];
@@ -52,8 +62,41 @@ public class GymActivity extends BackActivity {
         GymImagesRecyclerViewAdapter adapter = new GymImagesRecyclerViewAdapter(getApplicationContext(), data);
         mRecyclerViewEquipments.setAdapter(adapter);
 
-        setFavoriteImage();
-        mDatabase = FirebaseDatabase.getInstance().getReference("votes");
+        getFavoriteInfo();
+    }
+
+    private void getFavoriteInfo() {
+        mDatabaseVotes = FirebaseDatabase.getInstance().getReference("votes");
+        mDatabaseVotes.orderByChild("userId").equalTo(Utils.getUserUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    for (DataSnapshot post : dataSnapshot.getChildren()) {
+                        Log.d(TAG, "onDataChange; post: " + post);
+                        HashMap voteMap = (HashMap) post.getValue();
+                        //noinspection ConstantConditions
+                        if (voteMap != null && voteMap.containsKey("gymId") && voteMap.get("gymId").equals(mGym.getId())) {
+                            Log.d(TAG, "My favorite! S2");
+                            mIsFavorite = true;
+                            return;
+                        }
+                    }
+                    mIsFavorite = false;
+                    Log.d(TAG, "NOT favorite... yet");
+                } finally {
+                    setFavoriteImage();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled; post: " + databaseError);
+            }
+        });
+    }
+
+    private void setAccessibleText() {
+        mAccessibleText.setText(mGym.isPcdAble() ? R.string.gym_accessible_yes : R.string.gym_accessible_no);
     }
 
     @OnClick(R.id.gym_favorite)
@@ -66,7 +109,6 @@ public class GymActivity extends BackActivity {
             Log.d(TAG, "Searching gym with user = " + userId);
             removeFavorite();
         }
-        setFavoriteImage();
     }
 
     private void addFavorite() {
@@ -80,19 +122,43 @@ public class GymActivity extends BackActivity {
             vote.setUp(true);
             vote.setDate(new Date());
             Log.d(TAG, "Adding vote " + vote);
-            String voteKey = mDatabase.push().getKey();
-            mDatabase.child(voteKey).setValue(vote, (firebaseError, ref) -> {
-                if (firebaseError != null) {
-                    Log.e(TAG, "New vote could not be saved: " + firebaseError.getMessage());
-                } else {
-                    Log.d(TAG, "New vote saved successfully.");
-                }
-            });
+            String voteKey = mDatabaseVotes.push().getKey();
+            if (voteKey == null) {
+                Log.wtf(TAG, "voteKey is null. Weird");
+            } else {
+                mDatabaseVotes.child(voteKey).setValue(vote, (firebaseError, ref) -> {
+                    if (firebaseError == null) {
+                        Log.d(TAG, "New vote saved successfully.");
+                    } else {
+                        Log.e(TAG, "New vote could not be saved: " + firebaseError.getMessage());
+                    }
+                });
+            }
         }
     }
 
     private void removeFavorite() {
+        mDatabaseVotes = FirebaseDatabase.getInstance().getReference("votes");
+        mDatabaseVotes.orderByChild("userId").equalTo(Utils.getUserUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                            HashMap voteMap = (HashMap) postSnapshot.getValue();
+                            //noinspection ConstantConditions
+                            if (voteMap != null && voteMap.containsKey("gymId")
+                                    && voteMap.get("gymId").equals(mGym.getId())) {
+                                Log.d(TAG, "Value to be removed: " + voteMap);
+                                postSnapshot.getRef().removeValue();
+                            }
+                        }
+                    }
 
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Log.e(TAG, "Error trying to remove value: " + databaseError);
+                    }
+                });
     }
 
     private void setFavoriteImage() {
