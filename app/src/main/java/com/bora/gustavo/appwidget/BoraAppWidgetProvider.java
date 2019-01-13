6c7cac7bf91b07d5d9cee59b0c4fb1762285dae4
Gustavo Kieling <gustavoknz@gmail.com>
2019-01-12 20:57:22 -0400

    AppWidget; App icon replaced.

--- app/src/main/java/com/bora/gustavo/appwidget/BoraAppWidgetProvider.java
+++ app/src/main/java/com/bora/gustavo/appwidget/BoraAppWidgetProvider.java
@@ -5,21 +5,55 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.bora.gustavo.R;
 import com.bora.gustavo.activities.MainActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class BoraAppWidgetProvider extends AppWidgetProvider {
+    private static final String TAG = "BoraAppWidgetProvider";
+
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int appWidgetId : appWidgetIds) {
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference("gyms");
+        database.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.e(TAG, "Gyms retrieved from database: " + dataSnapshot.getChildrenCount());
+                long gymCount = dataSnapshot.getChildrenCount();
+                Log.d(TAG, "Total number of registered gyms: " + gymCount);
+                updateGymCount(gymCount);
+            }
 
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
-            views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+            private void updateGymCount(long gymCount) {
+                Intent intent;
+                PendingIntent pendingIntent;
+                RemoteViews views;
+                for (int appWidgetId : appWidgetIds) {
+                    intent = new Intent(context, MainActivity.class);
+                    pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+                    views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
+                    views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+                    if (gymCount >= 0) {
+                        views.setTextViewText(R.id.appwidget_text, context.getString(R.string.appwidget_text, gymCount));
+                    } else {
+                        views.setTextViewText(R.id.appwidget_text, context.getString(R.string.appwidget_text_empty));
+                    }
+                    appWidgetManager.updateAppWidget(appWidgetId, views);
+                }
+            }
 
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e(TAG, "The read failed: " + databaseError.getCode());
+                updateGymCount(-1);
+            }
+        });
     }
 }
